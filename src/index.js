47ec697f2000a130fab831df236d2c24f3812b18
Dipanshu <dipanshusahoo91@gmail.com>
2026-06-04 13:25:04 +0530

    Added healthcheck and database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/db.js";
 dotenv.config({
     path: "./.env"
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port http://localhost:${port}`);
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port http://localhost:${port}`);
+    });
+}).catch((error) => {
+    console.error("Error starting server:", error);
 });
